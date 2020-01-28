c195d81f7f228632e85f5b4ee779897db8061174
LaneDoyle <ldoyle@stcharlessd.org>
2020-01-28 12:06:43 -0600

    new file:   dictionary_reset.py 	modified:   game_library.py

---
+++ dictionary_reset.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Lane Doyle
+#1/28/20
+
+import pickle 
+
+'''Program to reset the dictionary'''
+
--- game_library.py
+++ game_library.py
@@ -9,7 +9,11 @@ import sys
 
 #Dictionary
 games = {1: ['FPS', 'Halo 3', 'Bungee', 'Microsoft', 'Xbox 360', '2007',
-             '10', 'either', '30.00', 'Yes', '1/15/2008', 'This game blows chunks!']
+             '10', 'either', '30.00', 'Yes', '1/15/2008', 'This game blows chunks!'],
+         2: ['Sandbox', 'Minecraft', 'Mojang', 'Mojang', 'Xbox One', '2011',
+             '10', 'either', '40.00', 'N/A', '3/15/2017', ''],
+         3: ['Action Role-Playing', 'Fallout 4', 'Bethesda Game Studios', 'Bethesda Softworks', 
+             'Xbox One', '2015', '7', 'Single', '70.00', 'Yes', '12/25/2015', 'Best game ever!']}
 
 #Functions
 def add_and_edit():

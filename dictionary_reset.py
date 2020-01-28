3b9685c23e1de8a47ad06873c8e6531c0145ceb7
LaneDoyle <ldoyle@stcharlessd.org>
2020-01-28 12:45:41 -0600

    modified:   dictionary_reset.py 	new file:   game_lib.pickle 	modified:   game_library.py

--- dictionary_reset.py
+++ dictionary_reset.py
@@ -6,3 +6,13 @@ import pickle
 
 '''Program to reset the dictionary'''
 
+games = {1: ['FPS', 'Halo 3', 'Bungee', 'Microsoft', 'Xbox 360', '2007',
+             '10', 'either', '30.00', 'Yes', '1/15/2008', 'This game blows chunks!'],
+         2: ['Sandbox', 'Minecraft', 'Mojang', 'Mojang', 'Xbox One', '2011',
+             '10', 'either', '40.00', 'N/A', '3/15/2017', ''],
+         3: ['Action Role-Playing', 'Fallout 4', 'Bethesda Game Studios', 'Bethesda Softworks', 
+             'Xbox One', '2015', '7', 'Single', '70.00', 'Yes', '12/25/2015', 'Best game ever!']}
+
+data_file = open("game_lib.pickle", "wb")
+pickle.dump(games, data_file)
+data_file.close()
--- game_library.py
+++ game_library.py
@@ -20,7 +20,23 @@ def add_and_edit():
     print("Running add_and_edit()")
 
 def print_all():
-    print("Running print_all()")
+    #print("Running print_all()")
+    games_key_list = games.keys()
+    for key in games_key_list:
+        print("-----------")
+        print("Genre: ", games[key][0])
+        print("Title: ", games[key][1])
+        print("Developer: ", games[key][2])
+        print("Publisher: ", games[key][3])
+        print("System: ", games[key][4])
+        print("Release Date: ", games[key][5])
+        print("Rating: ", games[key][6])
+        print("Single player/multi/either: ", games[key][7])
+        print("Price: ", games[key][8])
+        print("Beat it?: ", games[key][9])
+        print("Purchase Date: ", games[key][10])
+        print("Notes: ", games[key][11])
+        print("-----------")    
     
 def search_by_title():
     print("Running search_by_title()")
@@ -29,7 +45,11 @@ def remove_title():
     print("Running remove_title()")
     
 def save_library():
-    print("Running save_library()")
+    #print("Running save_library()")
+    data_file = open("game_lib.pickle", "wb")
+    pickle.dump(games, data_file)
+    data_file.close()    
+    print("File saved!")
     
 def quit():
     print("Running quit()")
